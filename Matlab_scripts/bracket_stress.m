d1fd66d403c3fedb7f79f053b54875fafd836b1c
punith reddy vanteddu <vpunithreddy@gmail.com>
2024-10-11 13:01:44 +0200

    update matlab scripts

--- Matlab_scripts/bracket_stress.m
+++ Matlab_scripts/bracket_stress.m
@@ -1,28 +1,22 @@
+close all
 %load and Initilalize PDE tool box and the stl file
 model = createpde("structural","static-solid");
-   g= importGeometry(model,"cogen_001_p_001.prt_ascii.stl");   
-% Figure of the loaded mesh(stl)
-%    figure
-%     pdegplot(model,"FaceLabels","on","VertexLabels","on") % to understand face and vertex lable distribution
-%     view(30,30);
-%     title("bracket")  
+g= importGeometry(model,"cogen_001_p_001.prt_ascii.stl");   
 %generate mesh fo the model
-    generateMesh(model);
-    ANGLE = 113
-    DISTANCE = 85.97
-    %evaluate the point of the centre where the force shall be acting upon
-    x_n = 23.0274 + DISTANCE*cosd(ANGLE-90)+15*sind(ANGLE-90);
-    y_n =0;
-    z_n = 15 + DISTANCE*sind(ANGLE-90)-15*cosd(ANGLE-90);
-    %obtain the face id of that particular point
-    FaceID = nearestFace(g,[x_n y_n z_n]);
-    % Designate the model mechanical properties
-    structuralProperties(model,"YoungsModulus",71.7e9,"PoissonsRatio",0.3);
-    % Apply constraints on the faces
-    structuralBC(model,"Face",[1 3 14],"Constraint","fixed");
-    % Apply pressure (Force per Area)
-    structuralBoundaryLoad(model,"Face", FaceID,"Pressure",250/0.00161365);
-    % Plot the mesh file
+generateMesh(model);
+%evaluate the point of the centre where the force shall be acting upon
+x_n = 23.0274 + DISTANCE*cosd(ANGLE-90)+15*sind(ANGLE-90);
+y_n =0;
+z_n = 15 + DISTANCE*sind(ANGLE-90)-15*cosd(ANGLE-90);
+%obtain the face id of that particular point
+FaceID = nearestFace(g,[x_n y_n z_n]);
+% Designate the model mechanical properties
+structuralProperties(model,"YoungsModulus",71.7e9,"PoissonsRatio",0.3);
+% Apply constraints on the faces
+structuralBC(model,"Face",[1 3 14],"Constraint","fixed");
+% Apply pressure (Force per Area)
+structuralBoundaryLoad(model,"Face", FaceID,"Pressure",250/0.00161365);
+% Plot the mesh file
 %     figure
 %     pdeplot3D(model)
 %     title("Mesh with Quadratic Tetrahedral Elements");   
--- Matlab_scripts/forearm_stress.m
+++ Matlab_scripts/forearm_stress.m
@@ -2,32 +2,22 @@ close all
 %load and Initilalize PDE tool box and the stl file
 model = createpde("structural","static-solid");
 g= importGeometry(model,"cogen_001_p_002.prt_ascii.stl");   
-LENGTH = 58
-AREA = 4010.99
-% Figure of the loaded mesh(stl)
-%    figure
-%     pdegplot(model,"FaceLabels","on",'FaceAlpha',0.5,"CellLabels","on") % to understand face distribution
-%     view(30,30);
-%     title("forearm")  
-   %evaluate the point of the centre where the force shall be acting upon
-    x_n = -(57.9+LENGTH);
-    y_n = 20;
-    z_n = 15;
-    AREA = AREA/1000000
-    %obtain the face id of that particular point
-    FaceID = nearestFace(g,[x_n y_n z_n]);
-    % Designate the model mechanical properties
-    structuralProperties(model,'Cell',1,"YoungsModulus",71.7e9,"PoissonsRatio",0.3);
-    % Apply constraints on the faces
-    structuralBC(model,"Face",1:14,"Constraint","fixed");   
-    % Apply surfacetraction
-    structuralBoundaryLoad(model,"Face", FaceID ,"Pressure",250/AREA);
-    % Plot the mesh file
-    generateMesh(model);
-%     figure
-%     pdeplot3D(model)
-%     title("Mesh with Quadratic Tetrahedral Elements");
-  
+%evaluate the point of the centre where the force shall be acting upon
+x_n = -(57.9+LENGTH);
+y_n = 20;
+z_n = 15;
+AREA = AREA/1000000
+%obtain the face id of that particular point
+FaceID = nearestFace(g,[x_n y_n z_n]);
+% Designate the model mechanical properties
+structuralProperties(model,'Cell',1,"YoungsModulus",71.7e9,"PoissonsRatio",0.3);
+% Apply constraints on the faces
+structuralBC(model,"Face",1:14,"Constraint","fixed");   
+% Apply surfacetraction
+structuralBoundaryLoad(model,"Face", FaceID ,"Pressure",250/AREA);
+% Plot the mesh file
+generateMesh(model);
+
 % solve the model  
 result = solve(model)
 stress_max = max(result.VonMisesStress)
